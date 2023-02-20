1ee54e0ce0ea8c6f86c3ba27ebc0d0ced5cc95b9
JuliaAvona <94717941+JuliaAvona@users.noreply.github.com>
2023-02-20 09:19:31 -0600

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
-    constructor() {}
+  #minNum = 0;
+  #maxNum = 0;
+  #guessNum = 0;
 
-    setRange(min, max) {
+  setRange(min, max) {
+    //этот метод принимает минимальное и максимальное значение диапазона чисел для угадывания
+    this.#minNum = min;
+    this.#maxNum = max;
+  }
 
-    }
+  guess() {
+    //этот метод возвращает кандидата решения (вы делаете предположение на основе диапазона и предыдущих предположений)
+    this.#guessNum = Math.round((this.#maxNum + this.#minNum) / 2);
+    return this.#guessNum;
+  }
 
-    guess() {
+  lower() {
+    //этот метод вызывается, если предыдущая функция guess() вернула число, которое больше, чем ответ
+    this.#maxNum = this.#guessNum;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    //этот метод вызывается, если предыдущий вызов функции guess() вернул число, которое меньше, чем ответ
+    this.#minNum = this.#guessNum;
+  }
 }
 
 module.exports = GuessingGame;
